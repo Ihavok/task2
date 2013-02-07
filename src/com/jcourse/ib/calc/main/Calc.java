93c63f450ac04f7dd8e5a80d3fc0f051ecf0acf7
Ihavok <ihavoker@gmail.com>
2013-02-07 17:55:04 +0700

    Update src/com/jcourse/ib/calc/main/Calc.java

--- src/com/jcourse/ib/calc/main/Calc.java
+++ src/com/jcourse/ib/calc/main/Calc.java
@@ -2,8 +2,7 @@ package com.jcourse.ib.calc.main;
 
 import com.jcourse.ib.calc.commands.*;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.*;
 
 /**
@@ -35,52 +34,104 @@ public class Calc
         }
     }
 
+    Map<String, Cmd> cmdMap = new HashMap<String, Cmd>();
+    Map<String, Double> defineMap = new HashMap<String, Double>();
+    Stack<Double> calcStack = new Stack<Double>();
 
-    public void main()
+    public Calc() throws Exception
     {
-        Map<String, Cmd> cmdMap = new HashMap<>();
-        initCmdMap(cmdMap);
+        initCmdMap();
 
-        Map<String, Double> defineMap = new HashMap<>();
-
-        Stack<Double> calcStack = new Stack<>();
-
-        List<String> commandsStr = read_commands();
-
-        for(String stroka : commandsStr)
-        {
-            String[] splitStr = null;
-            Cmd command = null;
-
-            stroka = stroka.trim();
-            splitStr = stroka.split(" ");
-            command =  cmdMap.get(splitStr[0]);
-
-            command.exec(
-                    splitStr,
-                    calcStack,
-                    defineMap
-            );
-        }
-
+        execute_commands_from_file();
     }
 
-    private List<String> read_commands()
+    public void execute_commands_from_console() throws IOException
+    {
+        String commandStr = "";
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        while( !(commandStr = input.readLine()).equalsIgnoreCase("Exit") )
+        {
+            execute_command(commandStr);
+        }
+    }
+
+    private void execute_commands_from_file()
+    {
+        List<String> commandsStr = read_commands_from_file();
+        execute_commands(commandsStr);
+    }
+
+    private void execute_commands(List<String> commandsStr)
+    {
+        for(String commandStr : commandsStr)
+        {
+            execute_command(commandStr);
+        }
+    }
+
+    private void execute_command(String commandStr)
+    {
+        String[] splitStr = null;
+        Cmd command = null;
+
+        commandStr = commandStr.trim().toUpperCase();
+        splitStr = commandStr.split(" ");
+
+        if (splitStr.length > 0)
+        {
+            if (cmdMap.containsKey(splitStr[0]))
+            {
+                command = cmdMap.get(splitStr[0]);
+
+                command.exec(
+                        splitStr,
+                        calcStack,
+                        defineMap
+                );
+            }
+            else
+            {
+                //todo
+            }
+        }
+        else
+        {
+            //todo
+        }
+    }
+
+    private List<String> read_commands_from_file()
     {
         List<String> stroki = new ArrayList();
-        stroki.add("DEFINE a 25");
-        stroki.add("PUSH a");
-        stroki.add("SQRT");
-        stroki.add("PRINT");
 
-        stroki.add("PUSH 4");
-        stroki.add("PUSH a");
-        stroki.add("+");
-        stroki.add("PRINT");
+        try
+        {
+            DataInputStream in = new DataInputStream(new FileInputStream("commands.txt"));
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String strLine;
+            while ((strLine = br.readLine()) != null)
+            {
+                if(strLine.length() > 0)
+                {
+                    stroki.add(strLine);
+                }
+            }
+            in.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            //e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            System.out.println("File commands.txt not found");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
         return stroki;
     }
 
-    private void initCmdMap(Map<String, Cmd> cmdMap)
+    private void initCmdMap()
     {
         if (!comandsProps.isEmpty())
         {
@@ -110,7 +161,7 @@ public class Calc
         }
         else
         {
-
+            //todo
         }
     }
 }
